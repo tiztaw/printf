3acdc5cb9afb3eb428eebcdafa8492d39aa8ab95
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-01-24 22:03:50 +0300

    Update main.h

--- test_files/main.h
+++ test_files/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(char *);
@@ -8,4 +9,5 @@ long long int power(int base, int x);
 long long convertToDecimal(int Number, int base);
 int print_binary(int number);
 int print_unsigned(unsigned int number);
+
 #endif
